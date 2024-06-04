b583aa754fed6b6b538f637a8f61bb7c7fcea7f1
Irwan Fathurrahman <meomancer@gmail.com>
2024-06-04 12:05:59 +0700

    Fix maputnik static url

--- django_project/core/settings/base.py
+++ django_project/core/settings/base.py
@@ -54,7 +54,7 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     absolute_path('core', 'static'),
     absolute_path('frontend', 'bundles'),
-    absolute_path('context_layer_management'),
+    absolute_path('context_layer_management', 'maputnik', 'dist'),
 )
 
 # Every cache key will get prefixed with this value - here we set it to
