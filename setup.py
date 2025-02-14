090ddb11a39363ae1afbf0c6a2ef41cee82c0b10
Danang <danangmassandy@gmail.com>
2025-02-14 14:59:28 +0000

    add drf yasg and nested routers to install_requires (#18)
    Co-authored-by: Dimas Ciputra <dimas@kartoza.com>

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
         'django-cors-headers==4.3.1',
         'GeoAlchemy2 >= 0.15.1',
         'geopandas >= 0.13.2',
+        'drf-yasg == 1.21.7',
+        'drf-nested-routers == 0.93.5',
         'fiona==1.10.1'
     ],
 )
