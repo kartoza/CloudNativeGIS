80ee88def1c0430e0f804d7d88065b1613597bfb
Irwan Fathurrahman <meomancer@gmail.com>
2025-12-22 19:11:35 +0700

    Add cleanup layer

--- django_project/cloud_native_gis/tasks.py
+++ django_project/cloud_native_gis/tasks.py
@@ -1,7 +1,11 @@
 # coding=utf-8
 """Cloud Native GIS."""
 
+import os
+from datetime import timedelta
+
 from celery.utils.log import get_task_logger
+from django.utils import timezone
 
 from core.celery import app
 
@@ -28,3 +32,19 @@ def process_layer_download(layer_download_id):
         layer_download.run()
     except LayerDownload.DoesNotExist:
         logger.error(f'LayerDownload {layer_download_id} does not exist')
+
+
+@app.task
+def cleanup_old_layer_downloads():
+    """Clean up layer download files older than 1 hour."""
+    from cloud_native_gis.models import LayerDownload
+
+    cutoff_time = timezone.now() - timedelta(hours=1)
+    old_downloads = LayerDownload.objects.filter(created_at__lt=cutoff_time)
+
+    for download in old_downloads:
+        if download.path and os.path.exists(download.path):
+            try:
+                os.remove(download.path)
+            except OSError:
+                pass
